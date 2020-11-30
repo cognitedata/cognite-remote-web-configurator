0623c5dba7af043773f8bb2cf93254c442d308bf
Mihil Ranathunga <mihilranathunga@gmail.com>
2020-11-30 16:16:27 +0530

    integrate server configs (#29)

--- src/components/Util/TwinItemContainer/TwinItemContainer.tsx
+++ src/components/Util/TwinItemContainer/TwinItemContainer.tsx
@@ -1,30 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './TwinItemContainer.module.scss';
 import { List } from 'antd';
 import { CogniteClient } from "@cognite/sdk";
 
-const onClickHandler = (onClick?: () => void) => {
-    if (!onClick) {
-        console.warn("function is not implemented");
-    }
-    else {
-        onClick();
-    }
-}
-
-export const TwinItemContainer: React.FC<{ sdk: CogniteClient, onClick?: () => void }> = (props) => {
+export const TwinItemContainer: React.FC<{ sdk: CogniteClient, onClick: (item: any) => void }> = (props) => {
 
     const [digitalTwins, setDigitalTwins]= useState<any[]>([]);
+    const digitalTwinConfigMap = useRef<Map<string, unknown> | null>(null);
     useEffect(()=>{
         (async ()=> {
             const response = await props.sdk.get(`${props.sdk.getBaseUrl()}/api/playground/projects/${props.sdk.project}/twins`);
             if(response) {
                 const twins = response.data.data?.items;
-                const twinNames = twins.map((twin: { id: number, data: { header: { name: string }}}) => twin?.data?.header?.name || twin.id);
+                const twinNames = [];
+                const twinMap = new Map();
+
+                for(const twin of twins) {
+                    const twinName = twin?.data?.header?.name || twin.id;
+                    twinNames.push(twinName);
+                    twinMap.set(twinName, twin);
+                }
                 setDigitalTwins(twinNames);
+                digitalTwinConfigMap.current = twinMap;
             }
         })();
     },[]);
+
+    const onListItemClick = (configName: string) => {
+        const configMap = digitalTwinConfigMap.current;
+        if(configMap && configMap.size > 0) {
+            const config = configMap.get(configName);
+            props.onClick(config);
+        }
+    }
 
     if(digitalTwins.length){
         return (
@@ -32,7 +40,7 @@ export const TwinItemContainer: React.FC<{ sdk: CogniteClient, onClick?: () => v
                 bordered
                 dataSource={digitalTwins}
                 renderItem={item => (
-                    <List.Item className={classes.twinItem} onClick={() => onClickHandler(props.onClick)}>
+                    <List.Item className={classes.twinItem} onClick={() => onListItemClick(item)} key={item}>
                         {item}
                     </List.Item>
                 )}
