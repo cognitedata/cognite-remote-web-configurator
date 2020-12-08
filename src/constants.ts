329ec2034d86aecb32dcb77349ff27ea0f8ca787
Mihil Ranathunga <mihilranathunga@gmail.com>
2020-12-09 04:02:30 +0530

    change dev mode

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "Cognite Remote Configurator";
 export const LOGIN_HEADER = "Enter Your Project Name";
 export const LOGIN_CDF_ENVIRONMENT_OPT_TEXT = "CDF Environment";
-export const DEV_MODE = true;
+export const DEV_MODE = false;
