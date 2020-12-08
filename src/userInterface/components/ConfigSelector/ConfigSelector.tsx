4ad2b81dd34d11c2f3672ef8fccd4b2d7a83f69a
Gayan Deegala <gayan@creativesoftware.com>
2020-12-08 16:57:44 +0530

    style for active twin

--- src/userInterface/components/ConfigSelector/ConfigSelector.tsx
+++ src/userInterface/components/ConfigSelector/ConfigSelector.tsx
@@ -5,17 +5,19 @@ import { CogniteClient } from "@cognite/sdk";
 
 export const ConfigSelector: React.FC<{ sdk: CogniteClient, onClick: (item: any) => void }> = (props) => {
 
-    const [digitalTwins, setDigitalTwins]= useState<any[]>([]);
+    const [digitalTwins, setDigitalTwins] = useState<any[]>([]);
+    const [selectedTwin, setSelectedTwin] = useState<any>();
     const digitalTwinConfigMap = useRef<Map<string, unknown> | null>(null);
-    useEffect(()=>{
-        (async ()=> {
+
+    useEffect(() => {
+        (async () => {
             const response = await props.sdk.get(`${props.sdk.getBaseUrl()}/api/playground/projects/${props.sdk.project}/twins`);
-            if(response) {
+            if (response) {
                 const twins = response.data.data?.items;
                 const twinNames = [];
                 const twinMap = new Map();
 
-                for(const twin of twins) {
+                for (const twin of twins) {
                     const twinName = twin?.data?.header?.name || twin.id;
                     twinNames.push(twinName);
                     twinMap.set(twinName, twin);
@@ -24,23 +26,24 @@ export const ConfigSelector: React.FC<{ sdk: CogniteClient, onClick: (item: any)
                 digitalTwinConfigMap.current = twinMap;
             }
         })();
-    },[]);
+    }, []);
 
     const onListItemClick = (configName: string) => {
         const configMap = digitalTwinConfigMap.current;
-        if(configMap && configMap.size > 0) {
+        if (configMap && configMap.size > 0) {
             const config = configMap.get(configName);
             props.onClick(config);
         }
+        setSelectedTwin(configName);
     }
 
-    if(digitalTwins.length){
+    if (digitalTwins.length) {
         return (
             <List
                 bordered
                 dataSource={digitalTwins}
                 renderItem={item => (
-                    <List.Item className={`${classes.twinItem}`} onClick={() => onListItemClick(item)} key={item}>
+                    <List.Item className={`${classes.twinItem} ` + (item === selectedTwin ? classes.selected: "")} onClick={() => onListItemClick(item)} key={item}>
                         {item}
                     </List.Item>
                 )}
