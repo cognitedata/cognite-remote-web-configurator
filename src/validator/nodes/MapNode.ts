8ea4b4196272b952c823b951d4892a71add5d381
malindakpt <malindakpt@gmail.com>
2020-12-23 02:17:35 +0530

    Fixed: Circular schema nodes validation (#105)
    * Fixed: Circular schema nodes validation
    
    * Code refactored

--- src/validator/nodes/MapNode.ts
+++ src/validator/nodes/MapNode.ts
@@ -3,10 +3,15 @@ import { ISchemaNode } from "../interfaces/ISchemaNode";
 import { BaseNode, IData } from "./BaseNode";
 
 export class MapNode extends BaseNode {
-    public sampleData: BaseNode;
-    
-    constructor(schema: ISchemaNode, data: IData, isRequired: boolean, sampleData: BaseNode){
-      super(DataType.map, schema, data, isRequired);
-      this.sampleData = sampleData;
-    }
+  public sampleData: BaseNode | undefined;
+
+  constructor(
+    schema: ISchemaNode,
+    data: IData,
+    isRequired: boolean,
+    sampleData: BaseNode | undefined
+  ) {
+    super(DataType.map, schema, data, isRequired);
+    this.sampleData = sampleData;
   }
+}
