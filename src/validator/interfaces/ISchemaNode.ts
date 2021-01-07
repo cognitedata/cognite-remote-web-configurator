ba1977309ef8deb2f9357be040a2fde2d21db551
Malinda Kumarasinghe <malindakpt@gmail.com>
2021-01-07 07:38:13 +0530

    Code refacored

--- src/validator/interfaces/ISchemaNode.ts
+++ src/validator/interfaces/ISchemaNode.ts
@@ -1,33 +1,41 @@
 export interface ISchemaNode {
-  properties?: { [key: string]: any };
-  type: string;
   description?: string;
+  properties?: { [key: string]: any };
+
+  // Type Related
+  type: string;
+  format?: string;
+  nullable?: boolean // Nullable is not handled for now. Since users have to include only required values and if its null, that property can be removed.
 
   // Common Props
+  example?: any;
   additionalProperties?: ISchemaNode;
   required?: string[];
-  minItems?: number;
-  maxItems?: number;
-  items?: ISchemaNode;
-  format?: string;
-  enum?: string[];
-  discriminator?: {
-    mapping: { [key: string]: string };
-    propertyName: string;
-  };
+
+  // Association Types
   allOf?: ISchemaNode[];
   anyOf?: ISchemaNode[];
   oneOf?: ISchemaNode[];
   not?: {
     type: string;
   };
-  example?: any;
+  discriminator?: {
+    mapping: { [key: string]: string };
+    propertyName: string;
+  };
 
   // StringNode
+  enum?: string[];
   pattern?: string;
   maxLength?: number;
 
   // NumberNode
   minimum?: number;
   maximum?: number;
+
+  // ArrayNode
+  uniqueItems?: boolean;
+  items?: ISchemaNode;
+  minItems?: number;
+  maxItems?: number;
 }
