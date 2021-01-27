84754af0c3c244f7ce6e164b3d669ed1d253d562
Gayan Deegala <Gayan@CreativeSoftware.com>
2021-01-27 13:30:24 +0530

    spinner

--- src/userInterface/components/CommandItem/CommandItem.tsx
+++ src/userInterface/components/CommandItem/CommandItem.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Button from "antd/es/button";
 import styles from "./CommandItem.module.scss";
+import Spin from 'antd/es/spin';
 
 export const CommandItem: React.FC<{ className?: string, onClick?: () => void, icon?: string, loading?: boolean }> = (props) => {
     console.log('asd loading:', props.loading);
+    const buttonProps = props.loading ?
+        {
+            disabled: true
+        } :
+        {
+            icon: props.icon,
+            onClick: props.onClick
+        }
 
     return (
         <Button
             type="primary"
             className={`${styles.commandBtn} ${props.className}`}
-            icon={props.icon}
             size="large"
-            onClick={props.onClick}>
-            {props.children}
+            {...buttonProps}
+        >
+            {props.loading ? <Spin size="small"/> : props.children}
         </Button>
     );
 }
